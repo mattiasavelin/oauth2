efea1f05d65bfcc434ef06464800e0062877076a
Mattias Avelin <mattias@dentech.se>
2025-07-25 19:28:07 +0200

    Update token.go

--- token.go
+++ token.go
@@ -13,7 +13,7 @@ import (
 	"strings"
 	"time"
 
-	"golang.org/x/oauth2/internal"
+	"github.com/mattiasavelin/oauth2/internal"
 )
 
 // defaultExpiryDelta determines how earlier a token should be considered
